1bf0ee3a0dd4c96ffb0e48e8294c70f96655ed04
Harrison Jenkins <jenhi001@mymail.unisa.edu.au>
2023-05-25 14:47:40 +0930

    Base files for assignment

---
+++ Main.py
@@ -0,0 +1,9 @@
+'''
+File: Main.py
+Description: This files contains the main code for the assignment.
+Author: Harrison Jenkins
+Student ID: 110348652
+EmailID: jenhi001
+This is my own work as defined by the University's Academic Misconduct Policy.
+'''
+
